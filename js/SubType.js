e6822acd00480b26df8c7cac7f76d310801bfb67
Jaya <1160196518@qq.com>
2018-12-04 19:18:10 +0800

    Update SubType.js
    添加寄生组合式继承

--- js/SubType.js
+++ js/SubType.js
@@ -11,26 +11,28 @@ function  SubType(name, age, weight) {
   this.weight = weight;
 }
 
-// ----------- 1 -------------
+// ----------- 1组合继承 -------------
 // SubType.prototype = new SuperType();
 // ---------------------------
 
 
-// ----------- 2 -------------
-SubType.prototype = Object.create(SuperType.prototype);
+// ----------- 2原型式继承 -------------
+// SubType.prototype = Object.create(SuperType.prototype);
+// ---------------------------
+
+
+// ----------- 3寄生组合式继承 -------------
+function inherit(Child, Parent) {
+  var prototype = Object.create(Parent.prototype);
+  prototype.constructor = Child;
+  Child.prototype = prototype;
+}
+inherit(SubType, SuperType);
+// ---------------------------
+
 SubType.prototype.getWeight = function() {
   console.log(this.weight);
 }
-// function inherit(Child, Parent) {
-//   var prototype = Object.create(Parent.prototype);
-//   console.log(111, prototype);
-//   prototype.constructor = Child;
-//   Child.prototype = prototype;
-// }
-
-// inherit(SubType, SuperType);
-// ---------------------------
-
-var sub = new SubType('jaya', 22, 55);
+var sub = new SubType('jaya', 22, 53);
 sub.getName();
 sub.getWeight();
